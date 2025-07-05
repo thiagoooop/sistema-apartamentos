f186b1a474d2464e35d50d2b0cdaffb1e8b94004
Thiago <thiago.pera@me.com>
2025-07-05 00:26:02 -0300

    build: desativa o experimento outputFileTracingRoot

--- next.config.js
+++ next.config.js
@@ -1,19 +1,8 @@
-const path = require('path');
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  distDir: process.env.NEXT_DIST_DIR || '.next',
-  output: process.env.NEXT_OUTPUT_MODE,
   experimental: {
-    outputFileTracingRoot: path.join(__dirname, '../'),
+    outputFileTracingRoot: undefined,
   },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: false,
-  },
-  images: { unoptimized: true },
 };
 
 module.exports = nextConfig;
